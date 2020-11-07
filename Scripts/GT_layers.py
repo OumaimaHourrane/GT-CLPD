7eaf087ae197cdbe0a29d6216f932d3e2f852221
Oumaima Hourrane <oumaima.hourrane@gmail.com>
2020-11-07 23:14:49 +0100

    Add files via upload

---
+++ Scripts/GT_layers.py
@@ -0,0 +1,29 @@
+import torch
+from torch import nn
+from torch.nn import Parameter
+import torch.nn.functional as F
+import math
+
+class GraphTransformer(nn.Module):
+
+    def __init__():
+    
+    
+    def forward():
+
+
+class GraphTransformerLayer(nn.Module):
+
+    def __init__():
+        
+
+ 
+    def forward():
+
+class RelationMultiheadAttention(nn.Module):
+    def __init__():
+        
+
+    def forward():
+    def forward():
+ 
---
+++ Scripts/encoders.py
@@ -0,0 +1,109 @@
+import numpy as np
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from transformer import Embedding
+import re
+
+def GraphEmbedding(vocab, embedding_dim, pretrained_file=None, amr=False, dump_file=None):
+    if pretrained_file is None:
+        return Embedding(vocab.size, embedding_dim, vocab.padding_idx)
+
+    tokens_to_keep = set()
+    for idx in range(vocab.size):
+        token = vocab.idx2token(idx)
+        # TODO: Is there a better way to do this? Currently we have a very specific 'amr' param.
+        if amr:
+            token = re.sub(r'-\d\d$', '', token)
+        tokens_to_keep.add(token)
+
+    embeddings = {}
+ 
+    if dump_file is not None:
+        fo = open(dump_file, 'w', encoding='utf8')
+
+    with open(pretrained_file, encoding='utf8') as embeddings_file:
+        for line in embeddings_file.readlines():    
+            fields = line.rstrip().split(' ')
+            if len(fields) - 1 != embedding_dim:
+                continue
+            token = fields[0]
+            if token in tokens_to_keep:
+                if dump_file is not None:
+                    fo.write(line)
+                vector = np.asarray(fields[1:], dtype='float32')
+                embeddings[token] = vector
+
+    if dump_file is not None:
+        fo.close()
+
+    all_embeddings = np.asarray(list(embeddings.values()))
+    print ('pretrained', all_embeddings.shape)
+    embeddings_mean = float(np.mean(all_embeddings))
+    embeddings_std = float(np.std(all_embeddings))
+    # Now we initialize the weight matrix for an embedding layer, starting with random vectors,
+    # then filling in the word vectors we just read.
+    embedding_matrix = torch.FloatTensor(vocab.size, embedding_dim).normal_(embeddings_mean,
+                                                                            embeddings_std)
+
+    for i in range(vocab.size):
+        token = vocab.idx2token(i)
+
+        # If we don't have a pre-trained vector for this word, we'll just leave this row alone,
+        # so the word has a random initialization.
+        if token in embeddings:
+            embedding_matrix[i] = torch.FloatTensor(embeddings[token])
+        else:
+            if amr:
+                normalized_token = re.sub(r'-\d\d$', '', token)
+                if normalized_token in embeddings:
+                    embedding_matrix[i] = torch.FloatTensor(embeddings[normalized_token])
+    embedding_matrix[vocab.padding_idx].fill_(0.)
+
+    return nn.Embedding.from_pretrained(embedding_matrix, freeze=False)
+
+class RelationEncoder(nn.Module):
+    def __init__(self, vocab, rel_dim, embed_dim, hidden_size, num_layers, dropout, bidirectional=True):
+      
+
+    def forward(self, src_tokens, src_lengths):
+        
+
+
+
+class TokenEncoder(nn.Module):
+    def __init__(self, token_vocab, char_vocab, char_dim, token_dim, embed_dim, filters, char2token_dim, dropout, pretrained_file=None):
+        
+
+    def forward(self, token_input, char_input):
+        
+class CNNEncoder(nn.Module):
+    def __init__(self, filters, input_dim, output_dim, highway_layers=1):
+        super(CNNEncoder, self).__init__()
+        self.convolutions = nn.ModuleList()
+        for width, out_c in filters:
+            self.convolutions.append(nn.Conv1d(input_dim, out_c, kernel_size=width))
+        final_dim = sum(f[1] for f in filters)
+        self.highway = Highway(final_dim, highway_layers)
+        self.out_proj = nn.Linear(final_dim, output_dim)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.normal_(self.out_proj.weight, std=0.02)
+        nn.init.constant_(self.out_proj.bias, 0.)
+
+    def forward(self, input):
+        # input: batch_size x seq_len x input_dim
+        x  = input.transpose(1, 2)
+        conv_result = []
+        for i, conv in enumerate(self.convolutions):
+            y = conv(x)
+            y, _ = torch.max(y, -1)
+            y = F.relu(y)
+            conv_result.append(y)
+
+        conv_result = torch.cat(conv_result, dim=-1)
+        conv_result = self.highway(conv_result)
+        return self.out_proj(conv_result) #  batch_size x output_dim
+
